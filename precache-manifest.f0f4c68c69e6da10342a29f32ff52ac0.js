05ce62150167b4005a134b58742e7adad805adff
irfanseikh2011 <seikhirfan2011@gmail.com>
2020-10-08 21:31:25 +0530

    Updates

---
+++ precache-manifest.f0f4c68c69e6da10342a29f32ff52ac0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17ad0c9fb88347ab7522f764debb265",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f9bf9ec3ddb533a25692",
+    "url": "/RoboFriends/static/css/main.08bc8d42.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/RoboFriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9bf9ec3ddb533a25692",
+    "url": "/RoboFriends/static/js/main.991b1323.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  }
+]);
