9aeecfd4ee116bf9cb4e13ef5f17b8396daea53c
irfanseikh2011 <seikhirfan2011@gmail.com>
2021-05-28 15:46:30 +0530

    Changed the class based approach to hooks

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import CardList from './CardList'
 import {robots} from './robots'
 import SearchBox from './SearchBox'
 import './App.css'
 import Scroll from './Scroll'
 
-class App extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            robots : robots,
-            searchField : ''
-        }
+function App() {
+
+    const [robot,setRobots] = useState([])
+    const [searchField,setSearchField] = useState('')
+
+
+    useEffect(() => {
+        setRobots(robots)
+    },[]);
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value})
-    }
+    const filteredRobots = robot.filter(robots => {
+        return robots.name.toLowerCase().includes(searchField.toLowerCase());
+    })
 
-    render(){
-            const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-            })
         return(
             <div className="tc baskerville">
                 <h1 className="f-5">Robo Friends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <CardList robots={filteredRobots}/>
                 </Scroll>
             </div>
         );
-    }
+
 }
 
 export default App;
